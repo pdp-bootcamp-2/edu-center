220ecd774bc8e8c303a61e34b59b95576306c047
furqattrade <furqattrade@mail.ru>
2019-05-16 12:34:18 +0500

    Furqat

--- src/main/java/uz/pdp/entity/Furqat.java
+++ src/main/java/uz/pdp/entity/Furqat.java
@@ -1,14 +1,4 @@
 package uz.pdp.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Furqat {
-    private String name;
-    private String familyName;
-    private String phone;
 }
