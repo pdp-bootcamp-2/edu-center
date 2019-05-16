b4fda1cf048e9135d226762af719041540f95c46
ilhomuslim <ilhomuslim@gmail.com>
2019-05-16 11:19:02 +0500

    Project created

---
+++ README.md
@@ -0,0 +1,2 @@
+# edu-center
+Education Center
---
+++ src/main/java/uz/pdp/entity/Ilhom.java
@@ -0,0 +1,14 @@
+package uz.pdp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ilhom {
+    private String name;
+    private String familyName;
+    private String phone;
+}
