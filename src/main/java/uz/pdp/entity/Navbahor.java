2f3a76ae2eda509692e7c6edd0ce06c412d8affd
bahora#1999 <qurbonovanavbahor8099@gmail.com>
2019-05-16 12:00:23 +0500

    flowers

---
+++ src/main/java/uz/pdp/entity/Navbahor.java
@@ -0,0 +1,14 @@
+package uz.pdp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Navbahor {
+    private String name;
+    private String familyName;
+    private String phone;
+}
